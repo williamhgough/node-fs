31b3201554bb7f40d46ec26f4e4070a5b8007e7d
William Gough <williamhenrygough@gmail.com>
2018-01-20 17:31:34 +0000

    add mongo url

--- config/keys.js
+++ config/keys.js
@@ -4,4 +4,7 @@ module.exports = {
             '764228509229-e32i7ef1b0pjeg0imsjb89maakpjrnca.apps.googleusercontent.com',
         clientSecret: '6iyYIP__JBu65NZaG_q3emqs',
     },
+    mlab: {
+        connect: 'mongodb://admin:admin@ds253587.mlab.com:53587/startup-survey',
+    },
 };
